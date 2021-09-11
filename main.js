dac5fb54dcbfa2dfa7fbda51c9a674dacb9b9efc
Durvasahuji <74409509+Durvasahuji@users.noreply.github.com>
2021-09-11 21:09:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+img="";
+
+function preload()
+{
+    img = loadimg ()
+}
+
+function setup
+{
+    canvas=createCanvas(350,360);
+    canvas.center();
+}
+
+function draw
+{
+    fill("#800000");
+    storke("#FFFF00");
+    rect(46,56,230,320);
+}
